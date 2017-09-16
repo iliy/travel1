bcd55746a1418df57fbbff33c2d1f4b93bd09dc0
R2D2 <iliy.kulichenko@gmail.com>
2017-09-16 10:21:52 +1000

    # Добавлены бандлы Осталось осуществить проверку angularjs

--- WebTrevel/App_Start/BundleConfig.cs
+++ WebTrevel/App_Start/BundleConfig.cs
@@ -10,7 +10,14 @@ namespace WebTrevel.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add()
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include("~/Scripts/jquery-1.10.2.min.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include("~/Scripts/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/angular").Include("~/Scripts/angular/angular.min.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/custom").Include("~/Scripts/custom.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/modernizr").Include("~/Scripts/modernizr-2.6.2.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.min.css"));
+            bundles.Add(new StyleBundle("~/Content/Site").Include("~/Content/Site.css"));
         }
     }
 }
