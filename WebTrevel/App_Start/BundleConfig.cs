8ecb1535bb29f6b60f00c2909381e7909cb52cdd
R2D2 <iliy.kulichenko@gmail.com>
2017-09-17 10:18:40 +1000

    # Проект настроен на работу с сервером Следующие: 1) Проверяем Ajax на angular 2) Набрасываем UI на работу с сервером

--- WebTrevel/App_Start/BundleConfig.cs
+++ WebTrevel/App_Start/BundleConfig.cs
@@ -10,10 +10,15 @@ namespace WebTrevel.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Clear();
+            bundles.ResetAll();
+
+            BundleTable.EnableOptimizations = false;
+
             bundles.Add(new ScriptBundle("~/Scripts/jquery").Include("~/Scripts/jquery-1.10.2.min.js"));
             bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include("~/Scripts/bootstrap.min.js"));
             bundles.Add(new ScriptBundle("~/Scripts/angular").Include("~/Scripts/angular/angular.min.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/custom").Include("~/Scripts/custom.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/custom").Include("~/Scripts/custom-1.01.js"));
             bundles.Add(new ScriptBundle("~/Scripts/modernizr").Include("~/Scripts/modernizr-2.6.2.js"));
 
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.min.css"));
